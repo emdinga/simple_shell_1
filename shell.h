c6bfd53828e34550746f2e1896adc30708b84328
elikimkwesi <elikimkwesi2003@gmail.com>
2023-04-23 19:49:53 -0700

    simple shell 1

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+#define MAX_XOMMAND_LENGHT 1024
+
+#endif
