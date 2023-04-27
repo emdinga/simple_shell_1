3a565d4dfde867aa831e1bb6f044d3d67fe5912c
emdinga <Emdinga@gmail.com>
2023-04-26 17:27:27 -0700

    simple_shell

--- shell.c
+++ shell.c
@@ -6,35 +6,39 @@
  * fork - create child process
  * Return: NULL if fgets encounters the end-of-file character, program exits
  */
-
 int main(void)
 {
-	char cmd[MAX_COMMAND_LENGTH];
+	char command[MAX_COMMAND_LENGTH + 1];
+	char *args[2];
+	pid_t pid;
 
 	while (1)
 	{
-		display_prompt();
-		if (fgets(cmd, MAX_COMMAND_LENGTH, stdin) == NULL)
+		printf("#cisfun$");
+		fflush(stdout);
+		if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
+		break;
+	}
+	command[strcspn(command, "\n")] = '\0';
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		args[0] = command;
+		args[1] = NULL;
+		if (execve(command, args, NULL) == -1)
 		{
-			printf("\n");
-			break;
-		}
-		cmd[strcmp(cmd, "\n")] = '\0'; /*remove trailing newlines*/
-
-		if (strcmp(cmd, "exit") == 0)
-		{
-			break;
-		}
-		if (cmd[0] == '/')
-		{
-			execute_command(&cmd[0]);
+			exit(EXIT_FAILURE);
 		}
 		else
 		{
-			char full_cmd[MAX_COMMAND_LENGTH + 10]; /*adding space for bin*/
+			int status;
 
-			snprintf(full_cmd, MAX_COMMAND_LENGTH + 10, "/bin/%s", cmd);
-			execute_command(&full_cmd[0]);
+			waitpid(pid, &status, 0);
 		}
 	}
 	return (0);
